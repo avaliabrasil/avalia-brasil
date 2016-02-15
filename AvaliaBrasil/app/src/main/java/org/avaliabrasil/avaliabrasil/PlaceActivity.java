657490cbfe752e346a2b97823482398899218fe5
Pedro Nascimento de Lima <pedrolima.n@gmail.com>
2016-02-15 15:02:25 -0200

    Place Activity adjustments

--- AvaliaBrasil/app/src/main/java/org/avaliabrasil/avaliabrasil/PlaceActivity.java
+++ AvaliaBrasil/app/src/main/java/org/avaliabrasil/avaliabrasil/PlaceActivity.java
@@ -1,9 +1,11 @@
 package org.avaliabrasil.avaliabrasil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class PlaceActivity extends AppCompatActivity {
 
@@ -27,5 +29,23 @@ public class PlaceActivity extends AppCompatActivity {
     private String getPlaceName(){
         // Temporário, enquanto não implementamos um template provider
         return getString(R.string.temp_place_name);
+    }
+
+    public void startEvaluationActivity (View view) {
+        Intent intent_evaluation_activity = new Intent(this, EvaluationActivity.class);
+        startActivity(intent_evaluation_activity);
+
+    }
+
+    public void startStatisticsActivity (View view) {
+        Intent intent_statistics_activity = new Intent(this, PlaceStatisticsActivity.class);
+        startActivity(intent_statistics_activity);
+
+    }
+
+    public void startNewPlaceActivity (View view) {
+        Intent intent_new_place_activity = new Intent(this, NewPlaceActivity.class);
+        startActivity(intent_new_place_activity);
+
     }
 }
