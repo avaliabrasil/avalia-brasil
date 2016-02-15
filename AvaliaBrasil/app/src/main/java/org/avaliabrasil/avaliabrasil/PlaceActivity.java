4e5de3355d1775ee4493245ce493aa78a863ee01
Pedro Nascimento de Lima <pedrolima.n@gmail.com>
2016-02-15 12:21:57 -0200

    Preparing getPLaceName method

--- AvaliaBrasil/app/src/main/java/org/avaliabrasil/avaliabrasil/PlaceActivity.java
+++ AvaliaBrasil/app/src/main/java/org/avaliabrasil/avaliabrasil/PlaceActivity.java
@@ -1,7 +1,6 @@
 package org.avaliabrasil.avaliabrasil;
 
 import android.os.Bundle;
-import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -13,22 +12,20 @@ public class PlaceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place);
 
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-
-       // toolbar.setTitle("UPA Zona Norte");
-
+        // Definindo o título da Toolbar
         CollapsingToolbarLayout toolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
-        //setSupportActionBar();
-        toolbarLayout.setTitle(getString(R.string.temp_place_name));
+        toolbarLayout.setTitle(getPlaceName());
 
-        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.app_bar);
-
+        // Ativando a opção voltar da Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        // Comentado vamos ver no que dá
-        // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    private String getPlaceName(){
+        // Temporário, enquanto não implementamos um template provider
+        return getString(R.string.temp_place_name);
     }
 }
