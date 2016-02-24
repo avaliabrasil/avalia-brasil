6becffe6512c8b67e0ad604667b8e3e4b80a0f53
Pedro Nascimento de Lima <pedrolima.n@gmail.com>
2016-02-24 14:07:08 -0300

    UriMatcher passing

--- AvaliaBrasil/app/src/androidTest/java/org/avaliabrasil/avaliabrasil/data/TestUriMatcher.java
+++ AvaliaBrasil/app/src/androidTest/java/org/avaliabrasil/avaliabrasil/data/TestUriMatcher.java
@@ -1,5 +1,6 @@
 package org.avaliabrasil.avaliabrasil.data;
 
+import android.content.UriMatcher;
 import android.net.Uri;
 import android.test.AndroidTestCase;
 
@@ -10,24 +11,22 @@ public class TestUriMatcher extends AndroidTestCase {
 
     private static final String GOOGLE_PLACE = "asdasgd8218hdddDdsSAD";
 
+
+    // Como Formas as Uris!
     private static final Uri TEST_PLACE_DIR = AvBContract.PlaceEntry.CONTENT_URI;
     private static final Uri TEST_PLACE_ITEM = AvBContract.PlaceEntry.buildGooglePlaceUri(GOOGLE_PLACE);
 
-    /*
-        Students: This function tests that your UriMatcher returns the correct integer value
-        for each of the Uri types that our ContentProvider can handle.  Uncomment this when you are
-        ready to test your UriMatcher.
-     */
-//    public void testUriMatcher() {
-//        UriMatcher testMatcher = WeatherProvider.buildUriMatcher();
-//
-//        assertEquals("Error: The WEATHER URI was matched incorrectly.",
-//                testMatcher.match(TEST_WEATHER_DIR), WeatherProvider.WEATHER);
-//        assertEquals("Error: The WEATHER WITH LOCATION URI was matched incorrectly.",
-//                testMatcher.match(TEST_WEATHER_WITH_LOCATION_DIR), WeatherProvider.WEATHER_WITH_LOCATION);
-//        assertEquals("Error: The WEATHER WITH LOCATION AND DATE URI was matched incorrectly.",
-//                testMatcher.match(TEST_WEATHER_WITH_LOCATION_AND_DATE_DIR), WeatherProvider.WEATHER_WITH_LOCATION_AND_DATE);
-//        assertEquals("Error: The LOCATION URI was matched incorrectly.",
-//                testMatcher.match(TEST_LOCATION_DIR), WeatherProvider.LOCATION);
-//    }
+
+    public void testUriMatcher() {
+        UriMatcher testMatcher = AvBProvider.buildUriMatcher();
+
+        assertEquals("Error: The URI was matched incorrectly.",
+                testMatcher.match(TEST_PLACE_DIR),AvBProvider.PLACES_LIST);
+
+        assertEquals("Error: The URI was matched incorrectly.",
+                testMatcher.match(TEST_PLACE_ITEM),AvBProvider.PLACE);
+
+    }
+
+    
 }
